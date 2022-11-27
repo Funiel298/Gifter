2bc21b6029942bd830d7b10e1e088dfcc45a7db6
Daniel <91716291+Funiel298@users.noreply.github.com>
2022-11-27 19:07:31 +0600

    Add files via upload

---
+++ devjs.js
@@ -0,0 +1,289 @@
+let products = {
+  data: [
+    {
+      productName: "The Complete Adult Psychotherapy Treatment Planner",
+      category: "Psychology",
+      price: "https://codepen.io/patrickhlauke/pen/vpQNgJ",
+      image: "https://m.media-amazon.com/images/P/B07R7DLNRM.01._SCLZZZZZZZ_SX500_.jpg",
+      link: "shorturl.at/goS89",
+    },
+    {
+      productName: "Ernie Ball Braided Guitar Cable",
+      category: "Music",
+      price: "49",
+      image: "https://cdn.mos.cms.futurecdn.net/uCXFaFXQNPd9k2TAxgogLW-1200-80.jpg.webp",
+      link: "https://www.shorturl.at/shortener.php",
+    },
+    {
+      productName: "Lady Justice Statue",
+      category: "Law",
+      price: "61",
+      image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1661528806-lady-justice-statue-1661528802.jpg?crop=1xw:1xh;center,top&resize=768:*",
+      link: "https://www.shorturl.at/shortener.php",
+    },
+    {
+      productName: "Gavel and Sound Round Block Set",
+      category: "Law",
+      price: "28",
+      image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1660840970-41PMsnYpmaL._SL500_.jpg?crop=0.760xw:1.00xh;0.114xw,0&resize=768:*",
+      link: "https://www.shorturl.at/shortener.php",
+    },
+    {
+      productName: "Barron's AP Psychology Flash Cards",
+      category: "Psychology",
+      price: "129",
+      image: "https://images-na.ssl-images-amazon.com/images/I/61EZQjxtESL._AC_UL600_SR600,600_.jpg",
+      link: "https://www.shorturl.at/shortener.php",
+    },
+    {
+      productName: "Back and Neck Massager",
+      category: "Programming",
+      price: "89",
+      image: "https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07QNN19XJ&Format=_SL500_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=szcz-20&language=en_US",
+      link: "https://www.shorturl.at/shortener.php",
+    },
+    {
+      productName: "Laptop Stickers Pack for Developer [50PCS] Programming Stickers of Front-end dev,Back-end Languages",
+      category: "Programming",
+      price: "10",
+      image: "https://m.media-amazon.com/images/I/61ZAkYMV81L._AC_SX679_.jpg",
+      link: "https://www.shorturl.at/shortener.php",
+    },
+    {
+      productName: "Indoor 3-Tier Relaxation Tabletop Fountain",
+      category: "Law",
+      price: "26",
+      image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1660838927-3fountain-1660838916.jpg?crop=1xw:1xh;center,top&resize=768:*",
+      link: "amazon.com/dp/B000QTUJXS?ots=1&slotNum=23&imprToken=4cc25c63-33a2-f91a-e90&linkCode=ogi&tag=bp_links-20&ascsubtag=%5Bartid%7C2089",
+    },
+    {
+      productName: "Sing Pendant Necklace",
+      category: "Singing",
+      price: "5",
+      image: "https://storage.googleapis.com/loveable.appspot.com/sing_pendant_fc1114591d/sing_pendant_fc1114591d.jpeg",
+      link: "https://storage.googleapis.com",
+    },
+    {
+      productName: "Songwriting Notebook",
+      category: "Singing",
+      price: "14",
+      image: "https://storage.googleapis.com/loveable.appspot.com/small_song_notebook_595fb34dd2/small_song_notebook_595fb34dd2.png",
+      link: "https://www.etsy.com/listing",
+    },
+    {
+      productName: "Cube 3",
+      category: "Magic",
+      price: "35",
+      image: "https://vinc.gumlet.io/gallery/thumbnails/cube-3.jpg?w=128&dpr=1.3",
+      link: "https://vinc.gumlet.io/gallery/thumbnails/cube-3.jpg?w=128&dpr=1.3",
+    },
+    {
+      productName: "The Dapper Deck Deluxe Set",
+      category: "Magic",
+      price: "150",
+      image: "https://vinc.gumlet.io/gallery/thumbnails/the-dapper-deck-deluxe-set.jpg?w=128&dpr=1.3",
+      link: "https://vinc.gumlet.io/gallery/thumbnails",
+    },
+    {
+      productName: "The Gold Standard",
+      category: "Magic",
+      price: "75",
+      image: "https://vinc.gumlet.io/gallery/thumbnails/the-gold-standard.jpg?w=128&dpr=1.3",
+      link: "https://vinc.gumlet.io/gallery",
+    },
+    {
+      productName: "Buddha Board",
+      category: "Artist",
+      price: "38",
+      image: "https://www.artlex.com/wp-content/uploads/2022/08/Buddha-Board-Art-Set.jpg",
+      link: "https://www.artlex.com/wp-content",
+    },
+    {
+      productName: "Artists Personalized Leather Paint Brush Roll",
+      category: "Artist",
+      price: "93",
+      image: "https://www.artlex.com/wp-content/uploads/2022/08/Classic-Artists-Roll-Mahi-Leather.webp",
+      link: "https://www.artlex.com/wp-content/uploads",
+    },
+    {
+      productName: "Crayola Dual-Tipped Markers",
+      category: "Artist",
+      price: "10",
+      image: "https://www.artlex.com/wp-content/uploads/2022/08/Crayola-Brush-Detail-Dual-Tip-Markers.jpg",
+      link: "https://www.artlex.com/wp-content/uploads/2022",
+    },
+    {
+      productName: "Victrola Suitcase Record Player",
+      category: "Music",
+      price: "60",
+      image: "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-260w,f_auto,q_auto:best/newscms/2022_43/3577682/416vooqvubl-sl500-6357f955d2745.jpg",
+      link: "https://www.artlex.com/wp-content/uploads/2022",
+    },
+    {
+      productName: "JBL Flip 5 Portable Speaker",
+      category: "Music",
+      price: "70",
+      image: "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-260w,f_auto,q_auto:best/newscms/2022_33/3567280/screen_shot_2022-08-18_at_1-13-35_pm.png",
+      link: "https://www.artlex.com/wp-content/uploads/2022",
+    },
+    {
+      productName: "Smart Moon Lamp",
+      category: "Male",
+      price: "200",
+      image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1655234356-114-1655234351.jpg?crop=1xw:1xh;center,top&resize=768:*",
+      link: "https://www.artlex.com/wp-content/uploads/2022",
+    },
+    {
+      productName: "Smart Writing Set",
+      category: "Male",
+      price: "280",
+      image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1661198980-87-1661198976.jpg?crop=1xw:1xh;center,top&resize=768:*",
+      link: "https://www.artlex.com/wp-content/uploads/2022",
+    },
+    {
+      productName: "Printable Custom Line Drawing",
+      category: "Female",
+      price: "30",
+      image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1635525663-il_794xN.3299937679_rz19.jpg?crop=1xw:1.00xh;center,top&resize=768:*",
+      link: "https://www.artlex.com/wp-content/uploads/2022",
+    },
+    {
+      productName: "Knit Loungewear Set",
+      category: "Female",
+      price: "34",
+      image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1658255638-gifts-for-her-gifts-for-women-matching-two-piece-sweat-suit-1658255582.jpg?crop=0.896xw:0.896xh;0.0673xw,0.0497xh&resize=768:*",
+      link: "https://www.artlex.com/wp-content/uploads/2022",
+    },
+    {
+      productName: "Star Sign Constellation Necklace",
+      category: "Female",
+      price: "34",
+      image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1555693045-il_570xN.1376060522_bf89.jpg?crop=1.00xw:0.677xh;0,0.153xh&resize=768:*",
+      link: "https://www.artlex.com/wp-content/uploads/2022",
+    },
+    {
+      productName: "4ft. Pre-Lit Riverside Pine Artificial Christmas Tree",
+      category: "Christmas",
+      price: "25",
+      image: "https://imgs.michaels.com/MAM/assets/1/5E3C12034D34434F8A9BAAFDDF0F8E1B/img/9D31D3546A694B1E83920A0CEF3FE616/10555064.jpg?fit=inside|1024:1024",
+      link: "https://www.artlex.com/wp-content/uploads/2022",
+    },
+    {
+      productName: "Black with Brushed Gold LED Snowman & Christmas Trees Snow Globe Lantern",
+      category: "Christmas",
+      price: "35",
+      image: "https://imgs.michaels.com/MAM/assets/1/5E3C12034D34434F8A9BAAFDDF0F8E1B/img/AFC950956F7A48F3A74E2DEB544E679B/D676585S_1.jpg?fit=inside|1024:1024",
+      link: "https://www.artlex.com/wp-content/uploads/2022",
+    },
+    {
+      productName: "Spa Gift Basket",
+      category: "Thanksgiving",
+      price: "43",
+      image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1666731409-51Wv5A7AxsS._SL500_.jpg?crop=1xw:1.00xh;center,top&resize=768:*",
+      link: "https://www.artlex.com/wp-content/uploads/2022",
+    },
+    {
+      productName: "Citrus Garland",
+      category: "Thanksgiving",
+      price: "98",
+      image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1626807969-089b5104-5a7f-41a4-afd2-8e8e9ae417a6--2018-1005_creekside-farms_citrus-garland_silo_ty-mecham_001.jpg?crop=0.800xw:1xh;center,top&resize=768:*",
+      link: "https://www.artlex.com/wp-content/uploads/2022",
+    },
+  ],
+};
+
+for (let i of products.data) {
+  //Create Card
+  let card = document.createElement("div");
+  //Card should have category and should stay hidden initially
+  card.classList.add("card", i.category, "hide");
+  //image div
+  let imgContainer = document.createElement("div");
+  imgContainer.classList.add("image-container");
+  //img tag
+  let image = document.createElement("img");
+  image.setAttribute("src", i.image);
+  imgContainer.appendChild(image);
+  image.style.width = '200px';
+  card.appendChild(imgContainer);
+  //container
+  let container = document.createElement("div");
+  container.classList.add("container");
+  //product name
+  let name = document.createElement("h5");
+  name.classList.add("product-name");
+  name.innerText = i.productName.toUpperCase();
+  container.appendChild(name);
+  //price
+  let price = document.createElement("h6");
+  price.innerText = "$" + i.price;
+  container.appendChild(price);
+  //link
+  var a = document.createElement('a'); 
+  var link = document.createElement("h6");
+  link.innerText = i.link;
+  a.appendChild(link); 
+  a.href = link; 
+  container.appendChild(a);
+
+  card.appendChild(container);
+  document.getElementById("products").appendChild(card);
+}
+
+//parameter passed from button (Parameter same as category)
+function filterProduct(value) {
+  //Button class code
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    //check if value equals innerText
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+
+  //select all cards
+  let elements = document.querySelectorAll(".card");
+  //loop through all cards
+  elements.forEach((element) => {
+    //display all cards on 'all' button click
+    if (value == "all") {
+      element.classList.remove("hide");
+    } else {
+      //Check if element contains category class
+      if (element.classList.contains(value)) {
+        //display element based on category
+        element.classList.remove("hide");
+      } else {
+        //hide other elements
+        element.classList.add("hide");
+      }
+    }
+  });
+}
+
+//Search button click
+document.getElementById("search").addEventListener("click", () => {
+  //initializations
+  let searchInput = document.getElementById("search-input").value;
+  let elements = document.querySelectorAll(".product-name");
+  let cards = document.querySelectorAll(".card");
+
+  //loop through all elements
+  elements.forEach((element, index) => {
+    //check if text includes the search value
+    if (element.innerText.includes(searchInput.toUpperCase())) {
+      //display matching card
+      cards[index].classList.remove("hide");
+    } else {
+      //hide others
+      cards[index].classList.add("hide");
+    }
+  });
+});
+
+//Initially display all products
+window.onload = () => {
+  filterProduct("all");
+};
